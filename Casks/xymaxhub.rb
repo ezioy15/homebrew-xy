1dde0071f24f055428e13140a1d0842174c294a4
ezioy15 <59588885+ezioy15@users.noreply.github.com>
2024-05-06 16:09:22 +0800

    Update xymaxhub.rb

--- Casks/xymaxhub.rb
+++ Casks/xymaxhub.rb
@@ -1,11 +1,12 @@
 cask 'xymaxhub' do
-  version '1.0'
-  sha256 :no_check
+  version '1.0.0'  # 版本号，请替换为实际版本
+  sha256 :no_check  # 必须替换为实际的SHA-256校验和
 
-  url 'http://172.18.1.123/app/xymaxhub.dmg'
-  name 'xymaxhub'
-  homepage 'https://www.maxhub.com/'
+  # 替换为实际的下载URL
+  url "http://172.18.1.123/app/xymaxhub.dmg"
+  name '传屏助手'
+  homepage 'https://www.maxhub.com/'  # 替换为应用的实际主页
 
-  app '/Applications/传屏助手.app'
+  app '传屏助手.app'
 
 end
