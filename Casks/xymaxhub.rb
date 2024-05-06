836a60aef68e7b10e9ee06d6e3dd3315efa25c6a
ezioy15 <59588885+ezioy15@users.noreply.github.com>
2024-05-06 15:58:22 +0800

    Add files via upload

---
+++ Casks/xymaxhub.rb
@@ -0,0 +1,11 @@
+cask 'xymaxhub' do
+  version '1.0'
+  sha256 :no_check
+
+  url 'http://172.18.1.123/app/xymaxhub.dmg'
+  name 'xymaxhub'
+  homepage 'https://www.maxhub.com/'
+
+  app '传屏助手.app'
+
+end
