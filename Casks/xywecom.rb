f771f341cdbca05c9a920d4e498da1f7e673ba7d
ezioy15 <59588885+ezioy15@users.noreply.github.com>
2024-05-06 15:51:33 +0800

    Add files via upload

---
+++ Casks/xywecom.rb
@@ -0,0 +1,11 @@
+cask 'xywecom' do
+  version '1.0'
+  sha256 :no_check
+
+  url 'http://172.18.1.123/app/xywecom.dmg'
+  name 'xywecom'
+  homepage 'https://work.weixin.qq.com/'
+
+  app '企业微信.app'
+
+end
