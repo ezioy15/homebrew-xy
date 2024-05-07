9228d47044900342dbd211e9fa0dafb95ed69612
ezioy15 <59588885+ezioy15@users.noreply.github.com>
2024-05-07 10:40:10 +0800

    Update xymaxhub.rb

--- Casks/xymaxhub.rb
+++ Casks/xymaxhub.rb
@@ -1,11 +1,9 @@
 cask 'xymaxhub' do
-  version '1.0.0'  # 版本号，请替换为实际版本
-  sha256 :no_check  # 必须替换为实际的SHA-256校验和
+  version '1.0.0'
+  sha256 :no_check
 
-  # 替换为实际的下载URL
   url "http://hb.ezioy.com/app/xymaxhub.dmg"
   name 'MAXHUBShare'
-  homepage 'https://www.maxhub.com/'  # 替换为应用的实际主页
 
   app 'MAXHUBShare.app'
 
