6c2b070b14d3b57e03b94d5a50c202dc6478374f
ezioy15 <59588885+ezioy15@users.noreply.github.com>
2024-05-07 10:34:49 +0800

    Update xywecom.rb

--- Casks/xywecom.rb
+++ Casks/xywecom.rb
@@ -2,7 +2,7 @@ cask 'xywecom' do
   version '1.0'
   sha256 :no_check
 
-  url 'http://172.18.1.123/app/xywecom.dmg'
+  url 'http://hb.ezioy.com/app/xywecom.dmg'
   name 'xywecom'
   homepage 'https://work.weixin.qq.com/'
 
