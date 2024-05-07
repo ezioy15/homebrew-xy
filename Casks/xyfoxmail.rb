03c33ca89ce6db3205955970215218656002e378
ezioy15 <59588885+ezioy15@users.noreply.github.com>
2024-05-07 10:39:29 +0800

    Update xyfoxmail.rb

--- Casks/xyfoxmail.rb
+++ Casks/xyfoxmail.rb
@@ -1,11 +1,9 @@
 cask 'xyfoxmail' do
-  version '1.0.0'  # 版本号，请替换为实际版本
-  sha256 :no_check  # 必须替换为实际的SHA-256校验和
+  version '1.0.0'
+  sha256 :no_check
 
-  # 替换为实际的下载URL
   url "http://hb.ezioy.com/app/xyfoxmail.dmg"
   name 'foxmail'
-  homepage 'https://www.foxmail.com/'  # 替换为应用的实际主页
 
   app 'Foxmail.app'
 
