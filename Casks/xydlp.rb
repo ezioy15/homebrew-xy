b69759f841817c0342adcbc8b0102fa79d6b65c3
ezioy15 <59588885+ezioy15@users.noreply.github.com>
2024-05-07 10:33:17 +0800

    Update xydlp.rb

--- Casks/xydlp.rb
+++ Casks/xydlp.rb
@@ -3,7 +3,7 @@ cask 'xydlp' do
   sha256 :no_check  # 必须替换为实际的SHA-256校验和
 
   # 替换为实际的下载URL
-  url "http://172.18.1.123/app/xydlp.dmg"
+  url "http://hb.ezioy.com/app/xydlp.dmg"
   name 'dlp'
   homepage 'https://www.foxmail.com/'  # 替换为应用的实际主页
 
