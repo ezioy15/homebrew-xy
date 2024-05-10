978fb65a401578a579c39e2c2b9f12fab702eb09
ezioy15 <59588885+ezioy15@users.noreply.github.com>
2024-05-10 11:59:10 +0800

    Add files via upload

---
+++ Casks/xyadobereader.rb
@@ -0,0 +1,10 @@
+cask 'xyadobereader' do
+  version '1.0.0'
+  sha256 :no_check
+
+  url "http://172.18.1.123/app/xyadobereader.dmg"
+  name 'xyadobereader'
+
+  pkg 'AcroRdrSCADC2400220687_MUI.pkg'
+
+end
