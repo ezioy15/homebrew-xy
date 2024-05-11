c041ffb9b4b0257d6d6e01a17a23623757c32573
ezioy15 <59588885+ezioy15@users.noreply.github.com>
2024-05-11 18:21:39 +0800

    Update xywecomarm.rb

--- Casks/xywecomarm.rb
+++ Casks/xywecomarm.rb
@@ -2,7 +2,7 @@ cask 'xywecomarm' do
   version '1.0'
   sha256 :no_check
 
-  url 'http://172.18.1.123/app/xywecomarm.dmg'
+  url 'http://hb.ezioy.com/app/xywecomarm.dmg'
   name 'xywecomarm'
 
   app '企业微信.app'
