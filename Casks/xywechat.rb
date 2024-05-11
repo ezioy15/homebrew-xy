f3b16555a7e26cb40db75348c4977de367f6725e
ezioy15 <59588885+ezioy15@users.noreply.github.com>
2024-05-11 18:26:20 +0800

    Update xywechat.rb

--- Casks/xywechat.rb
+++ Casks/xywechat.rb
@@ -2,7 +2,7 @@ cask 'xywechat' do
   version '1.0'
   sha256 :no_check
 
-  url 'http://172.18.1.123/app/xywechat.dmg'
+  url 'http://hb.ezioy.com/app/xywechat.dmg'
   name 'xywechat'
 
   app 'WeChat.app'
