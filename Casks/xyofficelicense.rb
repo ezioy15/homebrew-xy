5c7fe0b75862c659080af41c64f5c5e58d1b6663
ezioy15 <59588885+ezioy15@users.noreply.github.com>
2024-05-11 18:20:31 +0800

    Update xyofficelicense.rb

--- Casks/xyofficelicense.rb
+++ Casks/xyofficelicense.rb
@@ -2,7 +2,7 @@ cask 'xyofficelicense' do
   version '1.0'
   sha256 :no_check
 
-  url 'http://172.18.1.123/app/officelicense.pkg'
+  url 'http://hb.ezioy.com/app/officelicense.pkg'
   name 'xyofficelicense'
 
   pkg 'officelicense.pkg'
