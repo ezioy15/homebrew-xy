993d90827235ad6cbc05545f2812d45a77930131
ezioy15 <59588885+ezioy15@users.noreply.github.com>
2024-05-11 18:19:45 +0800

    Update xyoffice.rb

--- Casks/xyoffice.rb
+++ Casks/xyoffice.rb
@@ -2,7 +2,7 @@ cask 'xyoffice' do
   version '1.0'
   sha256 :no_check
 
-  url 'http://172.18.1.123/app/office.pkg'
+  url 'http://hb.ezioy.com/app/office.pkg'
   name 'xyoffice'
 
   pkg 'office.pkg'
