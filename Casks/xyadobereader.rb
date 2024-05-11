93701b096c72fde7dfedfb0ec53de6d118226fcd
ezioy15 <59588885+ezioy15@users.noreply.github.com>
2024-05-11 18:11:06 +0800

    Update xyadobereader.rb

--- Casks/xyadobereader.rb
+++ Casks/xyadobereader.rb
@@ -2,7 +2,7 @@ cask 'xyadobereader' do
   version '1.0.0'
   sha256 :no_check
 
-  url "http://172.18.1.123/app/xyadobereader.dmg"
+  url "http://hb.ezioy.com/app/xyadobereader.dmg"
   name 'xyadobereader'
 
   pkg 'AcroRdrSCADC2400220687_MUI.pkg'
