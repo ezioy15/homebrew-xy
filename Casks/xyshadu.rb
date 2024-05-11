1ac8387563dec55f65bacdfd2e6de8219fd90643
ezioy15 <59588885+ezioy15@users.noreply.github.com>
2024-05-11 18:21:26 +0800

    Update xyshadu.rb

--- Casks/xyshadu.rb
+++ Casks/xyshadu.rb
@@ -2,7 +2,7 @@ cask 'xyshadu' do
   version '1.0.0'
   sha256 :no_check
 
-  url "http://172.18.1.123/app/xyshadu.dmg"
+  url "http://hb.ezioy.com/app/xyshadu.dmg"
   name 'xyshadu'
 
   pkg 'tmsminstall.pkg'
