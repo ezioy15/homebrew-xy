1ef0a0ebf8ba087a44d040307a69f9ce049fafb6
ezioy15 <59588885+ezioy15@users.noreply.github.com>
2024-05-11 18:20:51 +0800

    Update xyprinter.rb

--- Casks/xyprinter.rb
+++ Casks/xyprinter.rb
@@ -2,7 +2,7 @@ cask 'xyprinter' do
   version '1.0.0'
   sha256 :no_check
 
-  url "http://172.18.1.123/app/xyprinter.dmg"
+  url "http://hb.ezioy.com/app/xyprinter.dmg"
   name 'xyprinter'
 
   pkg 'xiaoying_printer_10.80.1.205_8119_20230612173305.pkg'
