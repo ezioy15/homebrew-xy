49bd3e69b5bad25cb8411caa83261a6a06f6223e
ezioy15 <59588885+ezioy15@users.noreply.github.com>
2024-05-11 18:19:30 +0800

    Update xyndsc.rb

--- Casks/xyndsc.rb
+++ Casks/xyndsc.rb
@@ -2,7 +2,7 @@ cask 'xyndsc' do
   version '1.0.0'
   sha256 :no_check
 
-  url "http://172.18.1.123/app/xyndsc.dmg"
+  url "http://hb.ezioy.com/app/xyndsc.dmg"
   name 'xyndsc'
 
   pkg 'ndsc-installer-3.4.7-mac-x86_64-##172.18.4.5##9080##http##ace.pkg'
